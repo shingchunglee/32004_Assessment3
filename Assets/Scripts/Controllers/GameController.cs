f91f3a2facc5d71f7551093a021b0419dc0ecd79
shingchunglee <shingchung.lee@student.uts.edu.au>
2023-10-03 13:53:39 +1100

    - remove Tile0 - add generated map - change camera size based on map

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private Tweener tweener;
     // Start is called before the first frame update
     void Start()
     {
-        tweener = GetComponent<Tweener>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    public bool gameObjectMove(GameObject item) {
-        Movable moveComponent = item.GetComponent<Movable>();
-
-        return tweener.AddTween(
-            item.transform, 
-            item.transform.position,
-            new Vector3(item.transform.position.x + moveComponent.xVelocity, item.transform.position.y + moveComponent.yVelocity, 0.0f),
-            1f
-        );
     }
 }
